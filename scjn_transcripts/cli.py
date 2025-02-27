8a75a8df66a2e47012506dd9d2e3f28fa6dd2f6e
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2025-02-27 13:41:21 -0600

    Add dump command to CLI for exporting transcripts using TranscriptIoHandler

--- scjn_transcripts/cli.py
+++ scjn_transcripts/cli.py
@@ -5,6 +5,7 @@ import typer
 
 from scjn_transcripts.collector.transcripts import ScjnSTranscriptsCollector
 from scjn_transcripts.cleaner.transcripts import ScjnSTranscriptsCleaner
+from scjn_transcripts.io.transcripts import TranscriptIoHandler
 from scjn_transcripts.logger import logger
 
 app = typer.Typer(no_args_is_help = True)
@@ -43,6 +44,25 @@ def clean(
         await cleaner.close()
         
         logger.info(f"Cleaned {result} transcripts")
+
+    asyncio.run(main())
+
+@app.command()
+def dump(
+    output_dir: Annotated[str, typer.Argument(..., help = "Output directory")],
+    verbose: Annotated[bool, typer.Option("--verbose", "-v", help = "Increase verbosity")] = False,
+):
+    """Start the dumping process."""
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+
+    async def main():
+        io_handler = TranscriptIoHandler()
+        await io_handler.connect()
+        result = await io_handler.dump(output_dir)
+        await io_handler.close()
+        
+        logger.info(f"Dumped {result} transcripts to {output_dir}")
 
     asyncio.run(main())
 
